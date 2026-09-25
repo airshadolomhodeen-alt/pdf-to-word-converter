813e637a78539dce05ffe3363d3876c9f0aff6c5
airshadolomhodeen-alt <airshadolomhodeen@gmail.com>
2026-09-25 17:32:54 +0800

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,10 @@
+from pdf2docx import Converter
+
+pdf_file = 'PHASE 3 - Site Development Plan an'
+docx_file = 'airsadprop.docx'
+
+# Convert PDF to Word
+cv = Converter(pdf_file)
+cv.convert(docx_file, start=0, end=None)
+cv.close()
+print("Conversion complete!")
